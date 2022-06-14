41ff722fc04759acb6975395ed420773a8a583a1
Louvani <paula.louvani@gmail.com>
2022-06-14 14:34:17 -0500

    0. Inline styling

--- 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -2,27 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CourseListRow = ({isHeader, textFirstCell, textSecondCell}) => {
-	if (isHeader === true && textSecondCell == null) {
-		return (
-			<tr>
-				<th colSpan={2}>{textFirstCell}</th>
-			</tr>
-		);
-	} else if (isHeader === true && textSecondCell != null) {
-		return (
-			<tr>
-				<th>{textFirstCell}</th>
-				<th>{textSecondCell}</th>
-			</tr>
-		);
-	} else if (isHeader === false) {
-		return (
-			<tr>
-				<td>{textFirstCell}</td>
-				<td>{textSecondCell}</td>
-			</tr>
-		);
+	const styles = {
+		backgroundColor: '',
 	}
+	if (isHeader) {
+		styles.backgroundColor = '#deb5b545';
+		if (textSecondCell == null) {
+			return (
+				<tr style={styles}>
+					<th colSpan={2}>{textFirstCell}</th>
+				</tr>
+			);
+		} else {
+			return (
+				<tr style={styles}>
+					<th>{textFirstCell}</th>
+					<th>{textSecondCell}</th>
+				</tr>
+			);
+		}
+	}
+	styles.backgroundColor = '#f5f5f5ab';
+	return (
+		<tr style={styles}>
+			<td>{textFirstCell}</td>
+			<td>{textSecondCell}</td>
+		</tr>
+	);
 }
 
 CourseListRow.protoTypes = {
