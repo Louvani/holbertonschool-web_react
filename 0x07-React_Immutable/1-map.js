da7a443264aad259b8928e4619673a45e7eba7df
Louvani <paula.louvani@gmail.com>
2022-06-21 20:46:13 -0500

    task1

--- 0x07-React_Immutable/1-map.js
+++ 0x07-React_Immutable/1-map.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
 export default function getImmutableObject(object) {
-  const maped = Map(object);
-  return mapedd;
+  const map = Map(object);
+  return map;
 }
