30764449d610e1a0d8843162865cdeb034b68ab7
Louvani <paula.louvani@gmail.com>
2022-06-21 20:24:30 -0500

    with out test

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -5,11 +5,4 @@ function getImmutableObjec(object) {
   return map;
 }
 
-const test = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-}
-console.log(getImmutableObjec(test));
 export default getImmutableObjec;
