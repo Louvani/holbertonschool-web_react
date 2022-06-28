67230596a72c349fdae4eccbf220d17b43e75f4e
Louvani <paula.louvani@gmail.com>
2022-06-28 16:42:29 -0500

    responsive loging

--- 0x05-React_inline_styling/task_3/dashboard/src/Login/Login.js
+++ 0x05-React_inline_styling/task_3/dashboard/src/Login/Login.js
@@ -10,25 +10,44 @@ const Login = () => {
 				<p>
 					Login to access the full dashboard
 				</p>
-				<label htmlFor="email">Email: </label>
-				<input  className={css(styles.loginInput)} type="email" id="email" name="email" />
-				<label htmlFor="password">Password: </label>
-				<input className={css(styles.loginInput)} type="password" id="password" name="password" />
+				<label htmlFor="email">Email:</label>
+				<input type="email" id="email" className={css(styles.loginInput)} />
+				<label htmlFor="password">Password:</label>
+				<input type="password" id="password" className={css(styles.loginInput)} />
 				<button>OK</button>
 			</div>
 		</Fragment>
 	);
 }
 
+const screenSize = {
+  small: "@media screen and (max-width: 900px)",
+};
+
 const styles = StyleSheet.create({
-	loginBody: {
-		margin: "50px",
+  loginBody: {
+    margin: "50px",
     flexGrow: 1,
-	},
-	loginInput: {
-		marginLeft: '20px',
-		marginRight: '20px',
-	}
-})
+    [screenSize.small]: {
+      marginTop: "10px",
+      marginLeft: 0,
+      marginRight: 0,
+      marginBottom: 0,
+			display: 'flex',
+			flexDirection: 'column',
+    },
+  },
+
+  loginInput: {
+    marginLeft: "10px",
+    marginRight: "20px",
+    [screenSize.small]: {
+      display: "block",
+      marginLeft: 0,
+      marginTop: "10px",
+      marginBottom: "10px",
+    },
+  },
+});
 
 export default Login;
