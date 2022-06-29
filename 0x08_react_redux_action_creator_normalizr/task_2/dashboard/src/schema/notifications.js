c17461fcb91ed50ad0fcdf8e3babc3e5df9745a9
Louvani <paula.louvani@gmail.com>
2022-06-29 15:39:05 -0500

    2. Filter a normalized Schema

--- 0x08_react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
@@ -3,12 +3,6 @@ import { normalize, schema } from "normalizr";
 // const { normalize, schema } = require("normalizr");
 // const notificationsData = require('../../notifications.json')
 
-export function getAllNotificationsByUser(userId) {
-  return notificationsData.default.filter(
-      (item) => item.author.id === userId
-    ).map(({ context }) => context)
-}
-
 const user = new schema.Entity("users")
 const message = new schema.Entity("messages", {}, {idAttribute: 'guid'} )
 const notification = new schema.Entity("notifications", {
@@ -16,5 +10,21 @@ const notification = new schema.Entity("notifications", {
   context: message
 })
 const normalizedData = normalize(notificationsData.default, [notification]);
+
+export function getAllNotificationsByUser(userId) {
+
+  const notifications = normalizedData.entities.notifications;
+  const messages = normalizedData.entities.messages;
+  const userNotifications = [];
+
+  for (const item in notifications) {
+    if (notifications[item].author === userId) {
+      userNotifications.push(messages[notifications[item].context]);
+    }
+  }
+
+  return userNotifications;
+}
+
 
 export {normalizedData}
